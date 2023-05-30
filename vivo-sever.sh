6145331b2c5b1077304e8ecd49d2e18c3e77fe5d
Andreadote <andre.mj@yahoo.com>
2023-05-30 11:27:26 -0400

    vivo-sever.sh

---
+++ vivo-sever.sh
@@ -0,0 +1,16 @@
+ #!/bin/bash
+case $1 in
+  start)
+        echo started vivo server
+        echo  vivo sever has statment ;;
+   stop)
+        echo stop vivo sever
+        echo vivo has stop ;;
+restart)
+        echo vivo has restarted
+        echo has restarted ;;
+      *)
+        echo you have entered the wrong pin ;;
+    esac
+
+
